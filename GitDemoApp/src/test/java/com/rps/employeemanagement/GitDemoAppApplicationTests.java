cb84b88582fdb11dc772e3f7ff900fa627b1fe8c
rama <sandeepsomavarapu535@gmail.com>
2022-06-14 15:26:39 +0530

    first commit

---
+++ GitDemoApp/src/test/java/com/rps/employeemanagement/GitDemoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rps.employeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitDemoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
